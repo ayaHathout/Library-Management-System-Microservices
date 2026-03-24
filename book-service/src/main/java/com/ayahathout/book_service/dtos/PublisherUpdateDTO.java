4e64b308229a6949711a82e45d3378aafe272a04
Aya <ayahathout1@gmail.com>
2026-03-24 22:37:20 +0200

    enhance(book-service): validate publisher resources

--- book-service/src/main/java/com/ayahathout/book_service/dtos/PublisherUpdateDTO.java
+++ book-service/src/main/java/com/ayahathout/book_service/dtos/PublisherUpdateDTO.java
@@ -1,6 +1,7 @@
 package com.ayahathout.book_service.dtos;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.io.Serializable;
 public class PublisherUpdateDTO implements Serializable {
     private String name;
 
+    @Pattern(regexp = "^01[0125][0-9]{8}$", message = "Invalid phone number")
     private String phone;
 
     @Email(message = "Invalid email format")

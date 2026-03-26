e5432289dede2d1f6bbd8f664bce6635231df25d
Aya <ayahathout1@gmail.com>
2026-03-26 02:28:01 +0200

    feat(borrower-service): add borrower-service project

---
+++ borrower-service/src/test/java/com/ayahathout/borrower_service/BorrowerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayahathout.borrower_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrowerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

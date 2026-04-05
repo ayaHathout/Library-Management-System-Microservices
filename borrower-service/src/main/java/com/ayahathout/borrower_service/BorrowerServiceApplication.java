4f1e024da92c014f499b6aebfaac027703d4064a
Aya <ayahathout1@gmail.com>
2026-04-05 14:42:50 +0200

    refactor(borrower-service): enhance some codes

--- borrower-service/src/main/java/com/ayahathout/borrower_service/BorrowerServiceApplication.java
+++ borrower-service/src/main/java/com/ayahathout/borrower_service/BorrowerServiceApplication.java
@@ -2,8 +2,12 @@ package com.ayahathout.borrower_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.ayahathout.borrower_service.clients")
+@EnableScheduling
 public class BorrowerServiceApplication {
 
 	public static void main(String[] args) {
